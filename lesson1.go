ea23217bce48736f7014163d65efecf376b99aed
Cam <Cam.t.watson@gmail.com>
2024-05-01 16:24:15 -0400

    constants

--- lesson1.go
+++ lesson1.go
@@ -9,7 +9,24 @@ func name(){
 	fmt.Printf("Welcome %v! I will call you %v!\n", fname, nname,)
 }
 
+func input(){
+	city:= ""
+	var age int
+	fmt.Print("What city were you born in?")
+	fmt.Scanf("%s", &city)
+	fmt.Print("How old are you?")
+	fmt.Scanf("%v", &age)
+	fmt.Printf("You are %v and you were born in %s!\n", age, city)
+
+}
+
+func constants(){
+	const Country = "United States"
+	fmt.Printf("You are in the %v\n", Country)
+}
 
 func main() {
 	name()
+	input()
+	constants()
 }
